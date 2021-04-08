be719724d5b86517f7f2ac75b0282002e9a93cb7
Niklas Fiedler <git@nfiedler.de>
2021-04-08 23:26:51 +0200

    add kinect data loader

---
+++ data_utils/KinectDataLoader.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+import os
+import numpy as np
+import warnings
+import pickle
+
+from tqdm import tqdm
+from torch.utils.data import Dataset
+
+
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
+    pc = pc / m
+    return pc
+
+
+def farthest_point_sample(point, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [N, D]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:,:3]
+    centroids = np.zeros((npoint,))
+    distance = np.ones((N,)) * 1e10
+    farthest = np.random.randint(0, N)
+    for i in range(npoint):
+        centroids[i] = farthest
+        centroid = xyz[farthest, :]
+        dist = np.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = np.argmax(distance, -1)
+    point = point[centroids.astype(np.int32)]
+    return point
+
+
+class KinectDataLoader(Dataset):
+    def __init__(self, root, split='train', process_data=False):
+        self.root = root
+        self.path = os.path.join(self.root, split)
+        self.datapath = list()
+        self.label = list()
+
+        with open(os.path.join(self.path, 'labels.txt')) as f:
+            for line in f.readlines():
+                s = line.replace('\n', '').split(' ')
+                self.datapath.append(s[0])
+                self.label.append(s[1])
+
+    def __len__(self):
+        return len(self.datapath)
+
+    def _get_item(self, index):
+        return np.load(os.path.join(self.path, os.path.basename(self.datapath[index])))['points'], int(self.label[index])
+
+    def __getitem__(self, index):
+        return self._get_item(index)
+
+
+if __name__ == '__main__':
+    import torch
+    import argparse
+    import sys
+
+
+    def parse_args():
+        '''PARAMETERS'''
+        parser = argparse.ArgumentParser('Dataloader Test')
+        parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode')
+        parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
+        parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
+        parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
+        parser.add_argument('--num_category', default=40, type=int, choices=[10, 40], help='training on ModelNet10/40')
+        parser.add_argument('--epoch', default=200, type=int, help='number of epoch in training')
+        parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
+        parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
+        # parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training')
+        parser.add_argument('--log_dir', type=str, default=None, help='experiment root')
+        parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate')
+        parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
+        parser.add_argument('--process_data', action='store_true', default=False, help='save data offline')
+        parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
+        return parser.parse_args()
+
+    data = KinectDataLoader(sys.argv[1], split='train')
+    DataLoader = torch.utils.data.DataLoader(data, batch_size=12, shuffle=True)
+    for point, label in DataLoader:
+        print(point.shape)
+        print(label)
