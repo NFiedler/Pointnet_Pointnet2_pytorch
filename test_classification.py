d8d33fad88cf65a59e31a812879447b8c859c8bf
Niklas Fiedler <git@nfiedler.de>
2021-05-06 15:18:09 +0200

    write test results to file

--- test_classification.py
+++ test_classification.py
@@ -2,7 +2,8 @@
 Author: Benny
 Date: Nov 2019
 """
-from data_utils.ModelNetDataLoader import ModelNetDataLoader
+from data_utils.KinectDataLoader import KinectDataLoader
+from sklearn.metrics import classification_report, confusion_matrix
 import argparse
 import numpy as np
 import os
@@ -23,7 +24,7 @@ def parse_args():
     parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode')
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
     parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
-    parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
+    parser.add_argument('--num_category', default=2, type=int)
     parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
     parser.add_argument('--log_dir', type=str, required=True, help='Experiment root')
     parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
@@ -36,6 +37,8 @@ def test(model, loader, num_class=40, vote_num=1):
     mean_correct = []
     classifier = model.eval()
     class_acc = np.zeros((num_class, 3))
+    predictions = torch.tensor([]).cpu()
+    targets = torch.tensor([]).cpu()
 
     for j, (points, target) in tqdm(enumerate(loader), total=len(loader)):
         if not args.use_cpu:
@@ -49,6 +52,8 @@ def test(model, loader, num_class=40, vote_num=1):
             vote_pool += pred
         pred = vote_pool / vote_num
         pred_choice = pred.data.max(1)[1]
+        predictions = torch.cat((predictions, pred_choice.cpu()), 0)
+        targets = torch.cat((targets, target.cpu()), 0)
 
         for cat in np.unique(target.cpu()):
             classacc = pred_choice[target == cat].eq(target[target == cat].long().data).cpu().sum()
@@ -60,6 +65,12 @@ def test(model, loader, num_class=40, vote_num=1):
     class_acc[:, 2] = class_acc[:, 0] / class_acc[:, 1]
     class_acc = np.mean(class_acc[:, 2])
     instance_acc = np.mean(mean_correct)
+    report = classification_report(targets, predictions, labels=(0, 1))
+    conf_matrix = confusion_matrix(targets, predictions, labels=(0, 1))
+    print(report)
+    print(conf_matrix)
+    with open(args.log_dir + '_eval.txt', 'w') as f:
+        f.writelines([str(report), ' ', str(conf_matrix), ''])
     return instance_acc, class_acc
 
 
@@ -88,9 +99,9 @@ def main(args):
 
     '''DATA LOADING'''
     log_string('Load dataset ...')
-    data_path = 'data/modelnet40_normal_resampled/'
+    data_path = '/homeL/5fiedler/data/t3_t4_t5_prep/'
 
-    test_dataset = ModelNetDataLoader(root=data_path, args=args, split='test', process_data=False)
+    test_dataset = KinectDataLoader(root=data_path, split='test', include_normals=args.use_normals)
     testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=10)
 
     '''MODEL LOADING'''
