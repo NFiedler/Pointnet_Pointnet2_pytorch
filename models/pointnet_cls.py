a7d08ed18910fbc644fffe7f54ced5eb4db4fa7a
Niklas Fiedler <git@nfiedler.de>
2022-03-17 13:43:26 +0100

    pointnet with variable dropout and emb dims

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -4,17 +4,17 @@ import torch.nn.functional as F
 from pointnet_utils import PointNetEncoder, feature_transform_reguliarzer
 
 class get_model(nn.Module):
-    def __init__(self, k=40, normal_channel=True):
+    def __init__(self, num_class, k=40, dropout=0.4, emb_dims=1024, normal_channel=False):
         super(get_model, self).__init__()
         if normal_channel:
             channel = 6
         else:
             channel = 3
-        self.feat = PointNetEncoder(global_feat=True, feature_transform=True, channel=channel)
-        self.fc1 = nn.Linear(1024, 512)
+        self.feat = PointNetEncoder(global_feat=True, feature_transform=False, channel=channel, emb_dims=emb_dims)
+        self.fc1 = nn.Linear(emb_dims, 512)
         self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, k)
-        self.dropout = nn.Dropout(p=0.4)
+        self.fc3 = nn.Linear(256, num_class)
+        self.dropout = nn.Dropout(p=dropout)
         self.bn1 = nn.BatchNorm1d(512)
         self.bn2 = nn.BatchNorm1d(256)
         self.relu = nn.ReLU()
