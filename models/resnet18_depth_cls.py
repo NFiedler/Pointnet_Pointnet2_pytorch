f570cc79b3943e98a1fe8a7a21d10215a8d865f6
Niklas Fiedler <git@nfiedler.de>
2022-11-16 11:42:26 +0100

    new resnet model definitions

--- models/resnet18_depth_cls.py
+++ models/resnet18_depth_cls.py
@@ -17,32 +17,38 @@ from data_utils.MultiClothesDataLoader import MultiClothesDataLoader
 from typing import Iterator, Dict
 
 def get_model(num_class, config_dict):
-    return ResNet(num_class, config_dict['use_cpu'])
+    return ResNetDepth(num_class, config_dict['use_cpu'])
 
-class ResNet(nn.Module):
+class ResNetDepth(nn.Module):
     def __init__(self, num_class, use_cpu):
-        super(ResNet, self).__init__()
-        self.model_ft = models.resnet18(pretrained=True)
+        super(ResNetDepth, self).__init__()
+        self.model_ft = models.resnet50(pretrained=True)
         self.num_ftrs = self.model_ft.fc.in_features
         self.model_ft.fc = nn.Linear(self.num_ftrs, num_class)
+        self.model_ft.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        torch.nn.init.xavier_uniform(self.model_ft.conv1.weight)
+
         self.use_cpu = use_cpu
 
     def apply_transformations(self, input_tensor):
         preprocess = transforms.Compose([
             transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            # transforms.CenterCrop(224),
+            transforms.Normalize(mean=[0.5], std=[0.2]),
         ])
         return preprocess(input_tensor)
 
     def forward(self, data: Dict[MultiClothesDataLoader.Modalities, torch.Tensor]):
-        x: torch.Tensor = data[MultiClothesDataLoader.Modalities.RGB].float() / 255.0
+        x: torch.Tensor = data[MultiClothesDataLoader.Modalities.DEPTH].float()
+        x = torch.reshape(x, list(x.shape)+[1])
         x = x.transpose(1, 3)
         x = x.transpose(2, 3)
         x = self.apply_transformations(x)
         if not self.use_cpu:
             x = x.cuda()
-        return self.model_ft(x), None
+        x = self.model_ft(x)
+        x = F.log_softmax(x, dim=1)
+        return x, None
 
     def parameters(self, recurse: bool = True) -> Iterator:
         return self.model_ft.parameters(recurse)
